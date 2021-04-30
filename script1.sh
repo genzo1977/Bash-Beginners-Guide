068024a51e1c9d894e89f02f103db26ad61dd813
soti84 <soti84@outlook.com>
2021-04-30 20:01:20 +0100

    first commit

---
+++ script1.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+clear
+echo “The script starts now.“ 
+echo "Hi, $USER!“ 
+echo
+echo “I will new fetch yen a list cf connected users:"
+echo
+w
+echo  echo "I'm setting twc variables new." 
+COLOUR= “l:- 1 a c 1:: “  Elfg-‘LUE: ll IE1 ll  echo “This is a string: $CDLOUR“  echo “and this is a number: $vELUE“  echo  echo "I'm giving ycu back ycur prompt new.“ echo
+hjgh
