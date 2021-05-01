ca10d8a4f9f2bc82426d6156b4f48de43ae6cfc8
soti84 <soti84@outlook.com>
2021-05-01 23:45:59 +0100

    first commit

--- script1.sh
+++ script1.sh
@@ -7,5 +7,6 @@ echo “I will new fetch yen a list cf connected users:"
 echo
 w
 echo  echo "I'm setting twc variables new." 
-COLOUR= “l:- 1 a c 1:: “  Elfg-‘LUE: ll IE1 ll  echo “This is a string: $CDLOUR“  echo “and this is a number: $vELUE“  echo  echo "I'm giving ycu back ycur prompt new.“ echo
-hjgh
+COLOUR="black"
+VALUE="9"
+echo “This is a string: $CDLOUR“  echo “and this is a number: $vELUE“  echo  echo "I'm giving ycu back ycur prompt new.“ echo
